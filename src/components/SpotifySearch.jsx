54d8f210d505f32177998850dd5fa89b38075f3f
tomasvalentinotech <vrtomas1@gmail.com>
2025-08-31 23:53:53 -0300

    UI improve

--- src/components/SpotifySearch.jsx
+++ src/components/SpotifySearch.jsx
@@ -1,174 +1,179 @@
 import { useState } from 'react';
 import styled from 'styled-components';
 import { CgPlayButtonO } from 'react-icons/cg';
-import { HiStopCircle } from "react-icons/hi2";
-import { BsPlayCircleFill } from "react-icons/bs";
+import { HiStopCircle } from 'react-icons/hi2';
+import { BsPlayCircleFill } from 'react-icons/bs';
 
+// eslint-disable-next-line react/prop-types
+const App = ({ setCurrentSong }) => {
+    const [cancion, setCancion] = useState('');
+    const [canciones, setCanciones] = useState([]);
 
-const App = ({setCurrentSong}) => {
-  const [cancion, setCancion] = useState('');
-  const [canciones, setCanciones] = useState([]);
+    const handleSearch = async (e) => {
+        if (e.key === 'Enter') {
+            e.preventDefault();
+            if (cancion.trim() === '') {
+                alert('Debes ingresar algo');
+                return;
+            }
+            setCancion('');
+            await getSong(cancion);
+        }
+    };
 
-  const handleSearch = async (e) => {
-    if (e.key === 'Enter') {
-      e.preventDefault();
-      if (cancion.trim() === '') {
-        alert('Debes ingresar algo');
-        return;
-      }
-      setCancion('');
-      await getSong(cancion);
-    }
-  };
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key':
+                'f4fc2717fbmsh9bea989a671dd73p109b3ajsn036d6e612aa8',
+            'X-RapidAPI-Host': 'spotify23.p.rapidapi.com',
+        },
+    };
 
-  const options = {
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Key': 'baaffaef41msh3e4638ecc147a63p1c1ec6jsnd0d700e63d74',
-      'X-RapidAPI-Host': 'spotify23.p.rapidapi.com',
-    },
-  };
+    const getSong = async (cancion) => {
+        try {
+            const url = `https://spotify23.p.rapidapi.com/search/?q=${cancion}&type=multi&offset=0&limit=20&numberOfTopResults=5`;
 
-  const getSong = async (cancion) => {
-    try {
-      const url = `https://spotify23.p.rapidapi.com/search/?q=${cancion}&type=multi&offset=0&limit=20&numberOfTopResults=5`;
-      const data = await fetch(url, options);
-      const res = await data.json();
-      setCanciones(res.tracks.items);
-    } catch (error) {
-      console.log(`Error: ${error}`);
-    }
-  };
+            const data = await fetch(url, options);
+            const res = await data.json();
+            console.log('res', res);
+            setCanciones(res.tracks.items);
+        } catch (error) {
+            console.log(`Error: ${error}`);
+        }
+    };
 
-  const playsong = (cancion)=> {
-     const songToSet = {
-      title: cancion.data.name,
-      artist: cancion.data.artists.items[0].profile.name,
-      imagen: cancion.data.albumOfTrack.coverArt.sources[0].url
-    }
-    setCurrentSong(songToSet) 
-  }
+    const playsong = (cancion) => {
+        const songToSet = {
+            title: cancion.data.name,
+            artist: cancion.data.artists.items[0].profile.name,
+            imagen: cancion.data.albumOfTrack.coverArt.sources[0].url,
+        };
+        setCurrentSong(songToSet);
+    };
 
-  return (
-    <StyledContainer>
-  
-      <StyledForm>
-        <StyledInput
-          type='text'
-          value={cancion}
-          onChange={(e) => setCancion(e.target.value)}
-          onKeyPress={handleSearch}
-          placeholder='¿Qué te apetece escuchar?'
-        />
-      </StyledForm>
-      <StyledSongContainer>
-        {canciones.map((cancion, index) => (
-          <StyledSong key={index}>
-            <StyledSongImage src={cancion.data.albumOfTrack.coverArt.sources[0].url} alt='' />
-            <StyledSongTitle>{cancion.data.name}</StyledSongTitle>
-              <StyledPlayButton onClick={()=> playsong(cancion)}>
-                Play song <CgPlayButtonO className='botonPlay'/>
-              </StyledPlayButton>
-             <a href={cancion.data.uri}> 
-               <StyledPlayButton className='botonPlayTwo'>
-                Abrir en APP <CgPlayButtonO className='botonPlayTwo' />
-              </StyledPlayButton> 
-            </a> 
-          </StyledSong>
-        ))}
-      </StyledSongContainer>
-    </StyledContainer>
-  );
+    return (
+        <StyledContainer>
+            <StyledForm>
+                <StyledInput
+                    type="text"
+                    value={cancion}
+                    onChange={(e) => setCancion(e.target.value)}
+                    onKeyPress={handleSearch}
+                    placeholder="¿Qué te apetece escuchar?"
+                />
+            </StyledForm>
+            <StyledSongContainer>
+                {canciones.map((cancion, index) => (
+                    <StyledSong key={index}>
+                        <StyledSongImage
+                            src={
+                                cancion.data.albumOfTrack.coverArt.sources[0]
+                                    .url
+                            }
+                            alt=""
+                        />
+                        <StyledSongTitle>{cancion.data.name}</StyledSongTitle>
+                        <StyledPlayButton onClick={() => playsong(cancion)}>
+                            Play song <CgPlayButtonO className="botonPlay" />
+                        </StyledPlayButton>
+                        <a href={cancion.data.uri}>
+                            <StyledPlayButton className="botonPlayTwo">
+                                Abrir en APP{' '}
+                                <CgPlayButtonO className="botonPlayTwo" />
+                            </StyledPlayButton>
+                        </a>
+                    </StyledSong>
+                ))}
+            </StyledSongContainer>
+        </StyledContainer>
+    );
 };
 
 export default App;
 
 const StyledContainer = styled.div`
-  text-align: center;
-  padding: 20px;
+    text-align: center;
+    padding: 20px;
 `;
 
-
 const StyledForm = styled.form`
-  margin-top: 20px;
-  display: flex;
-  align-items: center;
-  justify-content: flex-start;
-  
+    margin-top: 20px;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
 `;
 
 const StyledInput = styled.input`
-  background-color: #121212;
-  border: 1px solid #1db954;
-  border-radius: 500px;
-  color: #fff !important;
-  height: 24px; 
-  padding: 6px 12px; 
-  width: 40%; 
-  margin-right: 10px;
-  margin-left: 10px; 
+    background-color: #121212;
+    border: 1px solid #1db954;
+    border-radius: 500px;
+    color: #fff !important;
+    height: 24px;
+    padding: 6px 12px;
+    width: 40%;
+    margin-right: 10px;
+    margin-left: 10px;
 `;
 
-
 const StyledSongContainer = styled.div`
-  margin-top: 20px;
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-around;
-  max-height: 60vh;
-  overflow-y: auto;
-  scrollbar-width: thin;
-  scrollbar-color: #282828 #121212;
-  &::-webkit-scrollbar {
-    width: 8px;
-  }
-  &::-webkit-scrollbar-thumb {
-    background-color: #282828;
-  }
+    margin-top: 20px;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+    max-height: 60vh;
+    overflow-y: auto;
+    scrollbar-width: thin;
+    scrollbar-color: #282828 #121212;
+    &::-webkit-scrollbar {
+        width: 8px;
+    }
+    &::-webkit-scrollbar-thumb {
+        background-color: #282828;
+    }
 `;
 
 const StyledSong = styled.div`
-  width: 200px;
-  margin: 10px;
-  text-align: center;
-  background-color: #181818;
-  padding: 16px;
-  border-radius: 8px;
-  transition: transform 0.2s ease-in-out;
+    width: 200px;
+    margin: 10px;
+    text-align: center;
+    background-color: #181818;
+    padding: 16px;
+    border-radius: 8px;
+    transition: transform 0.2s ease-in-out;
 
-  &:hover {
-    transform: scale(1.05);
-  }
+    &:hover {
+        transform: scale(1.05);
+    }
 `;
 
 const StyledSongImage = styled.img`
-  width: 100%;
-  border-radius: 8px;
+    width: 100%;
+    border-radius: 8px;
 `;
 
 const StyledSongTitle = styled.h2`
-  margin-top: 10px;
-  color: #fff;
-  font-size: 14px;
+    margin-top: 10px;
+    color: #fff;
+    font-size: 14px;
 `;
 
 const StyledPlayButton = styled.button`
-  margin-top: 5px;
-  padding: 6px;
-  cursor: pointer;
-  background-color: #1db954;
-  color: white;
-  border: none;
-  border-radius: 4px;
-  font-size: 12px;
-  font-family: 'Circular', 'Helvetica Neue', Arial, sans-serif; 
-  &.botonPlayTwo {
-    background-color:  #333; 
-    color: #fff; 
+    margin-top: 5px;
+    padding: 6px;
+    cursor: pointer;
+    background-color: #1db954;
+    color: white;
+    border: none;
+    border-radius: 4px;
+    font-size: 12px;
+    font-family: 'Circular', 'Helvetica Neue', Arial, sans-serif;
+    &.botonPlayTwo {
+        background-color: #333;
+        color: #fff;
 
-    &:hover {
-      background-color: #111; 
+        &:hover {
+            background-color: #111;
+        }
     }
-  }
-
 `;
